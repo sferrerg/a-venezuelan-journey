a57be712300f1b90b0c0e15f2abeefb61e8f9cf1
sferrerg <sferrerg@bu.edu>
2023-04-29 14:11:55 -0400

    Final Project Working Draft 2

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -195,7 +195,31 @@ function showHardestRoute() {
     let startingPoint = startingPoints[i];
     for (let j = 0; j < startingPoint.routes.length; j++) {
       let route = startingPoint.routes[j];
-      if (["Rio Hacha"].includes(route.destination)) {
+      if (route.destination === "Rio Hacha") {
+        let destinationMarker = L.marker(route.latlng).addTo(map);
+        destinationMarker.bindPopup("<b>" + route.destination + "</b><br>" + route.route);
+        bounds.push(route.latlng);
+      }
+    }
+  }
+  if (bounds.length > 0) {
+    let group = new L.featureGroup(bounds);
+    map.fitBounds(group.getBounds());
+  }
+}
+
+function showPlaneRoutes() {
+  map.eachLayer(function(layer) {
+    if (layer instanceof L.Marker || layer instanceof L.Popup) {
+      map.removeLayer(layer);
+    }
+  });
+  let bounds = [];
+  for (let i = 0; i < startingPoints.length; i++) {
+    let startingPoint = startingPoints[i];
+    for (let j = 0; j < startingPoint.routes.length; j++) {
+      let route = startingPoint.routes[j];
+      if (["Santo Domingo", "Panama", "Bogota", "Caracas"].includes(route.destination)) {
         let destinationMarker = L.marker(route.latlng).addTo(map);
         destinationMarker.bindPopup("<b>" + route.destination + "</b><br>" + route.route);
         bounds.push(route.latlng);
@@ -203,18 +227,79 @@ function showHardestRoute() {
     }
     ;
   }
-    map.fitBounds(bounds);
-  }
-
-
-function showPlaneRoutes() {
-
-}
+  map.fitBounds(bounds);
+};
 
 function showMixRoutes() {
-
-}
+  map.eachLayer(function(layer) {
+    if (layer instanceof L.Marker || layer instanceof L.Popup) {
+      map.removeLayer(layer);
+    }
+  });
+  let bounds = [];
+  for (let i = 0; i < startingPoints.length; i++) {
+    let startingPoint = startingPoints[i];
+    for (let j = 0; j < startingPoint.routes.length; j++) {
+      let route = startingPoint.routes[j];
+      if (["Rio Hacha", "Barranquilla", "Cartagena"].includes(route.destination)) {
+        let destinationMarker = L.marker(route.latlng).addTo(map);
+        destinationMarker.bindPopup("<b>" + route.destination + "</b><br>" + route.route);
+        bounds.push(route.latlng);
+      }
+    }
+    ;
+  }
+};
 
 function resetMap() {
+  map.eachLayer(function(layer) {
+    if (layer instanceof L.Marker) {
+      map.removeLayer(layer);
+    }
+    if (layer instanceof L.Polyline) {
+      map.removeLayer(layer);
+    }
+  });
+  let startingPoint = startingPoints[0];
+  let startingMarker = L.marker(startingPoint.latlng).addTo(map);
+  startingMarker.bindPopup("<b>" + startingPoint.name + "</b><br>Hi! You just clicked " + startingPoint.name + ", Venezuela. This is your starting point. This is where I lived before moving to the US. All the pins that just appeared are different routes you will need to take to get to the US. Click on another pin to explain how to get to Miami traveling through there!");
+  startingMarker.addEventListener('click', function(e) {
+    map.eachLayer(function(layer) {
+      if (layer instanceof L.Marker && layer !== e.target) {
+        map.removeLayer(layer);
+      }
+      if (layer instanceof L.Polyline) {
+        map.removeLayer(layer);
+      }
+    });
 
- }
+    let selectedDestinationMarker;
+    let destinationMarkers = [];
+    for (let j = 0; j < startingPoint.routes.length; j++) {
+      let route = startingPoint.routes[j];
+      let destinationMarker = L.marker(route.latlng).addTo(map);
+      destinationMarker.bindPopup("<b>" + route.destination + "</b><br>" + route.route);
+      destinationMarker.addEventListener('click', function(e) {
+        let startingLatLng = e.target._popup._source._latlng;
+        let destinationLatLng = e.latlng;
+        
+        map.eachLayer(function(layer) {
+          if (layer instanceof L.Marker && (layer !== e.target && layer !== selectedDestinationMarker && layer !== startingMarker)) {
+            if ((startingPoint.name === "Caracas" && (route.destination === "Santo Domingo" || route.destination === "Bogota")) ||
+                (startingPoint.name === "Rio Hacha" && (route.destination === "Bogota" || route.destination === "Cartagena" || route.destination === "Barranquilla"))) {
+            } else {
+              map.removeLayer(layer);
+            }
+          }
+        });
+        
+        selectedDestinationMarker = e.target;
+      });
+      destinationMarkers.push(destinationMarker);
+    }
+
+    let allMarkers = [startingMarker, ...destinationMarkers];
+    let bounds = L.latLngBounds(allMarkers.map(marker => marker.getLatLng()));
+    map.fitBounds(bounds);
+  });
+}
